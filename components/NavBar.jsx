f5c9b39893d5c0284df08141329a86d37cc834a4
santhosh2852 <santhoshmetheofficial@gmail.com>
2023-12-31 17:50:09 +0530

    Removed waste npm packages

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,17 +4,18 @@ import React from 'react'
 import Image from 'next/image'
 const NavBar = () => {
   return (
-    <div className='w-screen'>
-        <nav className='fixed flex justify-center items-center list-none md:text-3xl text-white bg-black w-full h-[100px] md:gap-[100px] text-center select-none mr-auto ml-auto text-xs gap-3'>
-            <li>EXPERIENCE</li>
-            <li>AWARDS</li>
+    <div className='w-full'>
+        <nav className='fixed flex justify-center items-center text-white bg-black w-screen h-[100px] md:text-2xl md:gap-10 text-center select-none mx-auto text-xs gap-5 px-10'>
+            <p>EXPERIENCE</p>
+            <p>AWARDS</p>
             <Image src='/white_logo.png' className=''
             draggable='false'
             width={80}
             height={80}
+            alt=''
             />
-            <li>ABOUT</li>
-            <li>CONTACT</li>
+            <p>ABOUT</p>
+            <p>CONTACT</p>
         </nav>
 
     </div>
--- components/PictureOnTraining.jsx
+++ components/PictureOnTraining.jsx
@@ -1,84 +1,23 @@
 'use client'
 import React, { Component } from 'react'
 import Image from 'next/image'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-// const PictureOnTraining = () => {
-//   return (
-//     <div className="slide-container">
-//       <div className='select-none'>
-//         <h1 className='bg-[url(https://www.apple.com/careers/images/fy21/apple_jobs_gradient_final_Apple_Jobs_Gradients_Full_Large/desktop@2x.png)] bg-clip-text text-transparent text-4xl font-semibold text-center pt-10 '>Picture On Training</h1>
-//       </div>
-//     </div>
-//   )
-// }
+// Default theme
+import '@splidejs/splide/css';
 
-// export default PictureOnTraining
-// function SampleNextArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{ ...style, display: "block", background: "red" }}
-//       onClick={onClick}
-//     />
-//   );
-// }
 
-// function SamplePrevArrow(props) {
-//   const { className, style, onClick } = props;
-//   return (
-//     <div
-//       className={className}
-//       style={{ ...style, display: "block", background: "green" }}
-//       onClick={onClick}
-//     />
-//   );
-// }
-export default class CustomArrows extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 1000, // Duration for each slide transition (in milliseconds)
-    autoplay: true, // Enables autoplay
-    autoplaySpeed: 3000, 
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow: <div />, // Rendering an empty div to hide the previous arrow
-    nextArrow: <div />, // Rendering an empty div to hide the next arrow
-    };
-    return (
+// or other themes
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+
+
+// or only core styles
+import '@splidejs/splide/css/core';
+const PictureOnTraining = () => {
+  return (
       <div className='pt-2'>
         <h1 className='bg-[url(https://www.apple.com/careers/images/fy21/apple_jobs_gradient_final_Apple_Jobs_Gradients_Full_Large/desktop@2x.png)] bg-cover bg-clip-text text-transparent text-4xl font-semibold text-center pt-10 '>Picture On Training</h1>
-        <Slider {...settings} className='pt-10 relative'>
-          <div>
-            <Image src='/pic1.jpg' className='mr-auto ml-auto flex'
-              height={500}
-              width={500}
-            />
-          </div>
-          <div>
-            <Image src='/pic2.jpg' className='mr-auto ml-auto flex'
-              height={500}
-              width={500}
-            />
-          </div>
-          <div>
-            <Image src='/pic3.jfif' className='mr-auto ml-auto flex'
-              height={500}
-              width={500}
-            />
-          </div>
-          <div>
-            <Image src='/pic4.jfif' className='mr-auto ml-auto flex'
-              height={500}
-              width={500}
-            />
-          </div>
-        </Slider>
+
       </div>
     );
-  }
 }
+export default PictureOnTraining
